552f7700c5e4a0b33322184abd601f9252abcde4
Priyanshu727 <priyanshumishra7272@gmail.com>
2024-05-07 18:34:42 +0530

    pr2
    to-do-list

--- pr2/index.js
+++ pr2/index.js
@@ -1,25 +1,80 @@
+// const { name } = require('ejs');
 const express = require('express');
 
-const port = 9008;
+const port = 8090;
 
 const app = express();
 
+app.use(express.urlencoded());
+
 app.set("view engine", "ejs");
 
+let userData = [
+    {
+        taskId: 1,
+        name: "Task 1",
+    },
+    {
+        taskId: 2,
+        name: "Taxk 2",
+    },
+]
 app.get('/', (req, res) => {
-    return res.render('index');
+    // console.log(userData);
+    return res.render('form', {
+        user: userData
+    });
 })
-app.get('/about', (req, res) => {
-    return res.render('about');
+app.post('/insertData', (req, res) => {
+    let editId = req.body.editId;
+
+    const { taskId, name } = req.body;
+    // console.log(editId);
+    if (editId) {
+        let data = userData.filter((curData) => {
+            if (curData.taskId == editId) {
+                curData.name = name;
+            }
+            return curData;
+        })
+
+        console.log("Data Updated Successfully..");
+        // res.send(data);
+         return res.redirect('/');
+
+    }
+
+    let obj = {
+        taskId: req.body.taskId,
+        name: req.body.name
+    }
+    userData.push(obj);
+    return res.redirect('back')
 })
-app.get('/contact', (req, res) => {
-    return res.render('contact');
+app.get('/deleteData', (req, res) => {
+    let taskId = req.query.taskId;
+    let data = userData.filter((val) => {
+        return val.taskId != taskId;
+    })
+    console.log(data);
+    userData = data
+    return res.redirect('back');
 })
+
+app.get('/editData', (req, res) => {
+    let taskId = req.query.taskId;
+    let data = userData.filter((val) => {
+        return val.taskId == taskId;
+    })
+    return res.render('editedData', { use: data[0] });
+})
+
+
 
 app.listen(port, (err) => {
     if (err) {
-        console.log("server is not working in port.")
+        console.log("server is not start in port");
         return false;
     }
-    console.log("server is start in port:-" + port);
+    console.log("server http://localhost:" + port);
 });
